f211561598338173182c2b10bd198e42b1e26ea2
damarismuthoni <muthonidamah@gmail.com>
2023-08-03 21:34:38 +0300

    first commit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# print statement for my name 
+print("My name is Muthoni Damaris")
+
+#print statement for Age
+print (" My Age is 23")
+
+# print statement expectations
+print(" Expectations include : Being Awesome at Python")
---
+++ variables.py
@@ -0,0 +1,18 @@
+things_lovable_1 = "Novels "
+things_lovable_2= "Music"
+
+motivation_1 = " i have a passion for coding"
+motivation_2 = " I think coding is cool"
+
+life_motto_1 = " Your now is not your forever "
+life_motto_2 = " your life is a story told about you, not one that you tell. "
+
+print  (things_lovable_1)
+print  (things_lovable_2)
+
+
+print (motivation_1)
+print(motivation_2)
+
+print (life_motto_1)
+print(life_motto_2)
