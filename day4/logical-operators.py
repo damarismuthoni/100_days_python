b5b902a6ab377ed55c5be1352b9a5b6c0691a09d
damarismuthoni <muthonidamah@gmail.com>
2023-08-10 16:27:47 +0300

    Day 7 comparison operators

--- day4/logical-operators.py
+++ day4/logical-operators.py
@@ -2,4 +2,4 @@ var1 = 10
 var2 = 32
  
 var = var1 or var2
-print(var)
+print(var)  
---
+++ day7/assignment/Task1.py
@@ -0,0 +1,32 @@
+
+# 7+5 >= 10
+var_1 = (7+5) >= 10
+print( "Ans to Q1 = " + str(var_1) )
+
+# “Anil” > “anil”
+var_2 = ("Anil") > ("anil")
+print("Ans to Q2 = " +str(var_2) )
+
+# 25 % 3 == 2
+var_3 = (25 % 3) == 2
+print ( "Ans to Q3 = " + str(var_3) )
+
+# 23 // 7  >= 9
+var_4 = (23 // 7) >= 9
+print ("Ans to Q4 = " + str(var_4))
+
+# 7 + 3 % 2 != True
+var_5 = (7 + 3 % 2) != True
+print ("Ans to Q5 =" + str(var_5)  )
+
+# False == False
+var_6 = False == False
+print("Ans to Q6 = " + str(var_6))
+
+# True != False
+var_7 = True!= False
+print("Ans to Q7 = " + str(var_7)) 
+
+# 29 == 89 % 30
+var_8 = 29 == (89 % 30)
+print("Ans to Q8 = " + str(var_8))
