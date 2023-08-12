8ff9fccb4ed68a6b0bf27c025a18d498ef1e04d0
damarismuthoni <muthonidamah@gmail.com>
2023-08-12 15:56:10 +0300

    data types assignment

---
+++ day9/DataTypesAssignment/Assignment.py
@@ -0,0 +1,28 @@
+# PYTHON DATA TYPES ASSIGNMENT
+# What is the output of the following code?
+
+x = 50
+def fun1():
+    x = 25
+    print(x)
+    
+fun1()
+print(x)
+
+
+#2. What is the output of the following variable assignment?
+
+x = 75
+def myfunc():
+    x = x + 1
+    print(x)
+
+myfunc()
+print(x)
+
+# 3. What is the output of the following code
+
+print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+
+# 4. What is the data type of print(type(0xFF))
+
