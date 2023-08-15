858dc12dbc9696383b6f23f5e9efb6dbcb1f1ffd
damarismuthoni <muthonidamah@gmail.com>
2023-08-15 22:10:38 +0300

    Transaction det and type

--- day11/assignment/task2.py
+++ day11/assignment/task2.py
@@ -1,1 +1,15 @@
-# - Write a python program that will prompt the user to perform the calculations of Area, surface area and volume of a cuboid
+# - Write a python program that will prompt the user to perform 
+#the calculations of Area, surface area and volume of a cuboid
+print("Cuboid calculations")
+print ("Input Length")
+
+len = int(input())
+
+#AREA
+area = len*len*len
+print("The area of the cuboid is: " + str(area))  
+
+#SURFACE AREA
+surfaceArea = 2*(len*len) + 2*(len*len) + 2*(len*len)
+print("The Surface Area of the cuboid is : " + str(surfaceArea))
+
--- day11/assignment/task3.py
+++ day11/assignment/task3.py
@@ -1,1 +1,23 @@
-# - Write a python program that will prompt the user to input their credit card information and check whether the information shared is a string or an integercd
+# - Write a python program that will prompt the user to 
+#input their credit card information and check whether the information shared is a string or an integercd
+
+name = "Christie"
+print("Hello " + name)
+print(" Please Enter the Following information to continue...")
+
+print("PIN")
+pin = input()
+
+print("Type of Transaction: Select One: ")
+print(" * Deposit * ")
+print(" * Withdrawal * ")
+print(" * Transfer * ")
+print(" * Balance * ")
+
+transaction = input()
+
+print(type(pin))
+print(type(transaction))
+
+
+
