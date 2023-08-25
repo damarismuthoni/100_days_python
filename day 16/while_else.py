9e0cb924ad696de337c1a9949bedec077d16f6f3
damarismuthoni <muthonidamah@gmail.com>
2023-08-25 09:58:41 +0300

    Day 16

---
+++ day 16/while_else.py
@@ -0,0 +1,20 @@
+# EXAMPLE 1
+    # FRUIT SHOPPING
+
+shopping_budget = 1500
+bananas = 0
+oranges = 0
+
+# Decreasing while loop
+
+while(shopping_budget >= 900):
+
+    shopping_budget = shopping_budget - 300
+    print(" The amount remaining is: ", shopping_budget)
+
+    bananas = bananas + 20
+    print("The bananas I have bought are", bananas)
+
+else:
+    oranges = oranges + 30
+    print("The oranges I have bought:" , oranges)
